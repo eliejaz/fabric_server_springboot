bf4c61de763818bd13e5ae091928e2d4dd6190d7
Elie.Azoury <Elie.Azoury@enghouse.com>
2022-07-13 16:04:15 +0300

    sfixed diagnosis

--- src/main/java/com/medicalRecord/hyperLedgerServer/Entity/Patient.java
+++ src/main/java/com/medicalRecord/hyperLedgerServer/Entity/Patient.java
@@ -24,7 +24,7 @@ public class Patient {
 	private String groupType ;
 	private Set<String>allergies= new HashSet<>() ;
 	private String emergencyContact ;
-	private List<String>Diagnosis =new ArrayList<>() ;
+	private List<Diagnosis>diagnosis =new ArrayList<>() ;
 	@Schema(description = "If kept empty will automatically add creator doctor.", required = true) 
 	private Set<String>doctorsId = new HashSet<>() ;
 	
